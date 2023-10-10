591217c6967ab09cf75ac0ab6ec66f1c4a2b53b6
Mojtaba Fazelinia <mfazelinia@gmail.com>
2023-10-10 20:38:25 +0330

    fixes Tokenomics

--- app/_sections/Tokenomics.tsx
+++ app/_sections/Tokenomics.tsx
@@ -1,11 +1,11 @@
 import React, {Fragment} from "react";
 import Heading from "../../components/typography/Heading";
 
-const FeatureCard = ({label, value}) => {
+const FeatureCard = ({label, value}: {label: string, value: string}) => {
     return (
         <div className="">
             <div className="text-sm text-secondary">{label}</div>
-            <div className="text-lg text-primary">{value}</div>
+            <div className="text-2xl text-primary">{value}</div>
         </div>
     )
 }
@@ -19,8 +19,8 @@ export default function Tokenomics() {
                 maxWidth="lg"
             />
             <div className="block">
-                <div className="grid grid-cols-2 gap-16">
-                    <div className="">
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-16">
+                    <div>
                         <p className="text-secondary text-base">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </p>
@@ -33,37 +33,38 @@ export default function Tokenomics() {
                     </div>
                 </div>
             </div>
-
             <div className="w-full bg-primary h-20 mt-8"/>
-            <div className="block">
-                <table className="w-full table -mt-20">
-                    <thead>
-                    <tr>
-                        <th>Token Allocation</th>
-                        <th>Number of Tokens</th>
-                        <th>% of Total Supply</th>
-                        <th>Price Per Token</th>
-                        <th>TGE Issuance (%)</th>
-                        <th>Emssion Frequency</th>
-                        <th>Cliff (months)</th>
-                        <th>Emission Length (MONTHS)</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {Array(8).fill(0).map((row,index) => (
+            <div className="container mx-auto px-0 md:px-8 lg:px-24">
+                <div className="w-full -mt-20 overflow-x-auto">
+                    <table className="w-full table">
+                        <thead>
                         <tr>
-                            <td>{"Auction House Staker Round"}</td>
-                            <td>{(75000000).toLocaleString()}</td>
-                            <td>{15}%</td>
-                            <td>{0.0015}</td>
-                            <td>{0}%</td>
-                            <td>{"Daily"}</td>
-                            <td>{1}</td>
-                            <td>{10}</td>
+                            <th>Token Allocation</th>
+                            <th>Number of Tokens</th>
+                            <th>% of Total Supply</th>
+                            <th>Price Per Token</th>
+                            <th>TGE Issuance (%)</th>
+                            <th>Emssion Frequency</th>
+                            <th>Cliff (months)</th>
+                            <th>Emission Length (MONTHS)</th>
                         </tr>
-                    ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        {Array(8).fill(0).map((row,index) => (
+                            <tr>
+                                <td>{"Auction House Staker Round"}</td>
+                                <td>{(75000000).toLocaleString()}</td>
+                                <td>{15}%</td>
+                                <td>{0.0015}</td>
+                                <td>{0}%</td>
+                                <td>{"Daily"}</td>
+                                <td>{1}</td>
+                                <td>{10}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </Fragment>
     )
