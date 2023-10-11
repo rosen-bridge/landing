3f6184a098de65ad4267ce1cd5adf7ee92753276
Mojtaba Fazelinia <mfazelinia@gmail.com>
2023-10-11 04:45:30 +0330

    fixes responsiveness and some else

--- app/_sections/RoadMap.tsx
+++ app/_sections/RoadMap.tsx
@@ -1,15 +1,15 @@
 import React, {Fragment} from "react";
 import Heading from "../../components/typography/Heading";
 
-const MapCard = ({number=1, right=false, children}) => {
+const MapCard = ({number=1, right=false, children}: {number: number, right?: boolean, children: any}) => {
     return (
-        <div className="mt-16 relative">
+        <div className="mt-8 md:mt-16 relative">
             <div className={`absolute ${right ? "start-0" : "end-0"} z-1 -translate-y-1/2 ${right ? "-translate-x-1/2" : "translate-x-1/2"} rounded-full bg-secondary color-quaternary font-bold flex justify-center items-center w-10 h-10`}>
                 Q{number}
             </div>
             <div className="h-2 bg-secondary rounded-tl-lg rounded-tr-lg"/>
-            <div className={`${right ? "ms-16" : "me-16"}`}>
-                <div className="bg-glass right:bg-primary rounded-bl-lg rounded-br-lg shadow-md p-8 flex flex-col gap-4">
+            <div className={`${right ? "ms-8 md:ms-16" : "me-16"}`}>
+                <div className="bg-glass right:bg-primary rounded-bl-lg rounded-br-lg shadow-md p-4 md:p-8 flex flex-col gap-4">
                     {children}
                 </div>
             </div>
@@ -17,7 +17,7 @@ const MapCard = ({number=1, right=false, children}) => {
     )
 }
 
-const MapItem = ({description, completed=false, inProgress=false}) => {
+const MapItem = ({description, completed=false, inProgress=false}: {description: string, completed?: boolean, inProgress?: boolean}) => {
     return (
         <div className="flex gap-4">
             <div className={`basis-4 text-xl ${completed ? "text-green-600" : "text-secondary"}`}>
@@ -33,9 +33,9 @@ const MapItem = ({description, completed=false, inProgress=false}) => {
     )
 }
 
-const MapYear = ({children}) => {
+const MapYear = ({children}: {children: number}) => {
     return (
-        <div className="mx-auto w-20 h-20 rounded-full bg-primary color-quaternary flex justify-center items-center text-lg	font-bold">
+        <div className="-ms-10 md:mx-auto w-20 h-20 rounded-full bg-primary color-quaternary flex justify-center items-center text-lg	font-bold">
             {children}
         </div>
     )
@@ -160,13 +160,32 @@ export default function RoadMap() {
                 title="Road map"
             />
             <StickyBackground/>
-            <div className="block grid grid-cols-2 mt-8">
+            <div className="md:hidden mt-8 ml-12 mr-6">
+                {roadMapData.map((section,index) => (
+                    <Fragment key={index}>
+                        <div>
+                            <MapYear>{section.year}</MapYear>
+                        </div>
+                        <div className="pb-8 border-s-2 border-dashed">
+                            <div className="h-1"/>
+                            {section.quarters.map((quarter,qIndex) => (
+                                <MapCard right number={qIndex+1} key={qIndex}>
+                                    {quarter.items.map((item,iIndex) => (
+                                        <MapItem key={iIndex} {...item}/>
+                                    ))}
+                                </MapCard>
+                            ))}
+                        </div>
+                    </Fragment>
+                ))}
+            </div>
+            <div className="block hidden md:grid grid-cols-2 mt-8">
                 {roadMapData.map((section,index) => (
                     <Fragment key={index}>
                         <div className="col-span-2">
                             <MapYear>{section.year}</MapYear>
                         </div>
-                        <div className="col-span-1 pb-8 border-e-2 border-dashed">
+                        <div className="col-span-1 pb-8 border-e-2 lg:border-e-4 border-dashed">
                             {section.quarters.filter(i => !i.right).map((quarter,qIndex) => (
                                 <MapCard number={2*qIndex+1} key={qIndex}>
                                     {quarter.items.map((item,iIndex) => (
