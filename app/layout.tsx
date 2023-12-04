664be3205334eca8227e043132278fbb597f634b
Mojtaba Fazelinia <mfazelinia@gmail.com>
2023-12-04 20:32:12 +0330

    fixes guard set slide show

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import '@iconscout/unicons/css/line.css';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
