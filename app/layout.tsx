aaafe689f3aa1b9b05b2c4934e1f94410dd4149d
Mohammad Kermani <mkermani144@gmail.com>
2023-12-16 14:36:22 +0330

    fix: add GA scripts only if the corresponding env var is set

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,16 +23,22 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
                 {children}
                 <Footer/>
             </body>
-            <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`} />
-            <Script id="google-analytics">
-                {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
+            {
+                process.env.NEXT_PUBLIC_GA_ID && (
+                    <>
+                        <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`} />
+                        <Script id="google-analytics">
+                            {`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
 
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-                `}
-            </Script>
+                            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+                            `}
+                        </Script>
+                    </>
+                )
+            }
         </html>
     )
 }
